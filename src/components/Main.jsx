b46e4a076d7def12620643e3be696172e8e6d59d
Eshan <2016mehrab@gmail.com>
2025-05-16 23:49:51 +0600

    update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import { CardWithForm } from './CardWithForm'
-import Heading from './Heading';
+import React, { useEffect } from "react";
+import { CardWithForm } from "./CardWithForm";
+import Heading from "./Heading";
+import { toast } from "sonner";
 
 const Main = (props) => {
-const padding= "px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-10"
+  const padding = "px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-10";
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      toast(
+        "The backend may take a few seconds to start. If it fails, try again."
+      );
+    }, 2000); 
+    return () => clearTimeout(timer);
+
+  }, []);
   return (
-    <div className={`${padding} flex flex-col gap-8 justify-center items-center` } {...props}>
+    <div
+      className={`${padding} flex flex-col gap-8 justify-center items-center`}
+      {...props}
+    >
       <Heading>Audio to Text Converter</Heading>
-      <CardWithForm/>
+      <CardWithForm />
     </div>
+  );
+};
 
-  )
-}
-
-export default Main
+export default Main;
